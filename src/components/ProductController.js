fe80ed4bb8733f175084d8ae1db54c4f94893647
Reid Ashwill <reidashwill@gmail.com>
2020-07-03 08:06:59 -0700

    create ProductCotroller with constructor and beginings of a render method.  productList is an empty array inside state.

---
+++ src/components/ProductController.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class ProductControl extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      productList: []
+    }
+  }
+
+  render(){
+    let currentyVisibleState = null;
+    let buttonText = null;
+    
+
+}
